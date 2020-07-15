d32b5736c1b582d60799b79f46371e39fc13ea04
unknown <william.henke@computingforall.org>
2020-07-15 14:50:36 -0700

    The password part works and will tell you if the password is too short or too long.

---
+++ form.js
@@ -0,0 +1,37 @@
+let name = document.getElementById('name');
+let password = document.getElementById('password');
+name.addEventListener('keyup', validName);
+password.addEventListener('keyup', validPassword);
+
+// function isSubstring() {
+//     namecheck.every(function(substring)
+// }
+function validName() {
+    let nameCheck = document.getElementById('name').value;
+
+    if(1===1){
+        console.log('green');
+        // display red x mark
+    }
+    else {
+        console.log('red');
+        // display the green check mark
+    }
+}
+
+function validPassword() {
+    let passwordCheck = document.getElementById('password').value;
+
+    if(passwordCheck.length >= 8 && passwordCheck.length <= 12) {
+        document.getElementById('invalidPassword').style.display = "none";
+        // console.log('yes');
+        // display red x mark
+    }
+    else {
+        document.getElementById('invalidPassword').style.display = "block";
+        // console.log('no');
+        // display green check mark
+    }
+}
+// var poa = str.search("space";)
+// poa < name.length
